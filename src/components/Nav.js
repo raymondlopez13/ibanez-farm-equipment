6be76952520363b396369ed06b19640ff8dadb23
Raymond Lopez <raymondlopez13@yahoo.com>
2021-09-21 18:02:01 -0500

    heroku deploy

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Nav() {
     return (
       <nav>
-          <a href='/'><img src='/images/logo.png' className='home-logo' alt="Logo"/></a>
+          <a href='/'><img src='/public/images/logo.png' className='home-logo' alt="Logo"/></a>
           <ul className='nav'>
               <li><a href='/about'>About</a></li>
               <li><a href='/contact'>Contact</a></li>
